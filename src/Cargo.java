1f220dd8514b45dac40f2b8cf91b1b790d24cbfb
DanielCS17 <102037535+DanielCS17@users.noreply.github.com>
2022-04-21 13:30:18 +0100

    SeFeature: ha añadido el método load a la clase Cargo

--- src/Cargo.java
+++ src/Cargo.java
@@ -1,10 +1,28 @@
 public class Cargo extends SpaceShip{
 
     private int cargaMaxima;
+    private int cargaActual = 0;
+    private int contenedoresParaCargar;
 
     public Cargo(String nombre, String matriculaGalactica, int aceleracion, int ejeMovimiento, int cargaMaxima) {
         super(nombre, matriculaGalactica, aceleracion, ejeMovimiento);
         this.cargaMaxima = cargaMaxima;
+    }
+
+    public void load() {
+        // Comprobación
+        int contenedoresTrasCargar = cargaActual + contenedoresParaCargar;
+
+        if (contenedoresTrasCargar <= cargaMaxima) {
+            cargaActual += contenedoresParaCargar;
+            System.out.println("Se han cargado " + contenedoresParaCargar + " contenedores, la carga actual es:  " + cargaActual);
+
+        } else if (cargaActual == cargaMaxima){
+            System.out.println("ALERTA: La capacidad de carga de la nave está al máximo.");
+
+        } else {
+            System.out.println("ERROR: El valor introducido es demasiado grande.");
+        }
     }
 
     public int getCargaMaxima() {
